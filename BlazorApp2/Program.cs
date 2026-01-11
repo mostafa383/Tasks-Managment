7016b2cf997db14cfefe87f7766d7f172f136054
Your Name <mostafaomeromer@gmail.com>
2026-01-11 19:59:20 +0300

    Push filter tasks filter by data

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -1,10 +1,21 @@
 using BlazorApp2;
 using BlazorApp2.Components;
 using BlazorApp2.Data;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+builder.Services.AddScoped<Blazorise.Modules.IJSDatePickerModule, Blazorise.Modules.JSDatePickerModule>();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
--- BlazorApp2/TaskDto.cs
+++ BlazorApp2/TaskDto.cs
@@ -6,7 +6,7 @@
         public string? Description { get; set; }
         public Pirority Pirority { get; set; }
         public TaskType TaskType { get; set; }
-        public DateTime StartDate { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; }
         public int Position { get; set; }
     }
